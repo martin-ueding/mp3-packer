bb2187f59fbfd8381751f5757f682641f62f8236
Martin Ueding <dev@martin-ueding.de>
2011-04-16 17:47:30 +0200

    added engine option to choose between lame and ffmpeg

--- mp3_compressor.py
+++ mp3_compressor.py
@@ -11,8 +11,12 @@ import sys
 def main():
 	parser = optparse.OptionParser(usage=gettext("%prog path"), description=gettext("Traverses through a folder and creates a copy of the folder structure but compresses everything to a certain bitrate."))
 	parser.add_option("--bitrate", dest="bitrate", type="int", default=128, help=gettext("bitrate in kbit [default: %default]"))
+	parser.add_option("--engine", dest="engine", default="ffmpeg", help=gettext("engine to use (ffmpeg or lame) [default: %default]"))
 
 	(options, args) = parser.parse_args()
+
+	global engine
+	engine = options.engine
 
 	if len(args) != 1:
 		print gettext("call with -h to get help")
@@ -38,7 +42,7 @@ def main():
 def parsefolder(bitrate, dirname, names):
 	for name in names:
 		# TODO add support for AAC too
-		if name[-4:] == ".mp3":
+		if name[-4:] == ".mp3" or name[-4:] == ".m4a":
 			infile = dirname+"/"+name
 			outfile = destpath+dirname+"/"+name
 			if not os.path.exists(outfile):
@@ -62,7 +66,11 @@ def encode(bitrate, infile, outfile):
 
 	# encode the file
 	print gettext("encoding %s") % infile
-	command = 'lame --quiet -b %d "%s" "%s"' % (bitrate, infile, outfile)
+	if engine == "lame":
+		command = 'lame --quiet -b %d "%s" "%s"' % (bitrate, infile, outfile)
+	if engine == "ffmpeg":
+		command = 'ffmpeg -i "%s" -acodec libmp3lame -ac 2 -ab %dk "%s"' % (infile, bitrate, outfile)
+
 	os.system(command)
 
 
